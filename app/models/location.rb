b5e2e908ced206e02b0a644ad2b2ba8cf161e02a
Mutabazi C <charl.mutabazi@gmail.com>
2015-09-28 18:53:45 +0900

    added fields on location document/table

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,9 +2,12 @@ class Location
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :user_id, type: String
+  field :user_id, type: Integer
+  field :username, type: String
   field :floor_name, type: String
-  field :status, type: String
+  field :uuid, type: String
+  field :major, type: String
+  field :minor, type: String
   field :motion_data, type: String, :default => "(0,0,0)"
 
 end
